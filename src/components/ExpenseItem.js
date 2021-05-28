0761afcc75ed62d8b53742d067b2ac66f08dc11d
Kacper Makowski <kacper.makowski1@gmail.com>
2021-05-28 20:59:42 +0200

    Push data to component by props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(data) {
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{data.date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{data.title}</h2>
+        <div className="expense-item__price">${data.price}</div>
       </div>
     </div>
   );
