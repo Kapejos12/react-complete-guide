478a69717e826f7468636308f7e589290de413e9
Kacper Makowski <kacper.makowski1@gmail.com>
2021-05-29 16:45:47 +0200

    Changes in Expenses component. Added Expenses styles.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,9 @@
 import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
 
 function Expenses(props){
     return(
-        <div>
+        <div className="expenses">
             <ExpenseItem title={props.expenses[0].title} price={props.expenses[0].price} date={props.expenses[0].date}/>
             <ExpenseItem title={props.expenses[1].title} price={props.expenses[1].price} date={props.expenses[1].date}/>
             <ExpenseItem title={props.expenses[2].title} price={props.expenses[2].price} date={props.expenses[2].date}/>
