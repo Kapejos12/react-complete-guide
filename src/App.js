7564d607de5f22b65d773bb0416773cdf7b77b23
Kacper Makowski <kacper.makowski1@gmail.com>
2021-05-30 17:46:14 +0200

    Managment form inputs tate with useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
 const expenses = [
@@ -10,24 +11,8 @@ const expenses = [
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
-      {/* <ExpenseItem 
-        title={expenses[0].title} 
-        price={expenses[0].price} 
-        date={expenses[0].date}/>
-      <ExpenseItem 
-        title={expenses[1].title} 
-        price={expenses[1].price} 
-        date={expenses[1].date}/>
-      <ExpenseItem 
-        title={expenses[2].title} 
-        price={expenses[2].price} 
-        date={expenses[2].date}/>
-      <ExpenseItem 
-        title={expenses[3].title} 
-        price={expenses[3].price} 
-        date={expenses[3].date}/> */}
     </div>
   );
 }
