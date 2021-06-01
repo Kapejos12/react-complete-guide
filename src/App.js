6d8702f6d34e22484e96333b4b9f3b304e05d26f
Kacper Makowski <kacper.makowski1@gmail.com>
2021-06-01 15:21:24 +0200

    Create add new expense function in app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+import { useState } from 'react';
 
-const App = () => {
-const expenses = [
+const INIT_EXPENSES = [
   {id:'e1', title: 'Car Insurance', price: 294.67, date: new Date(2020,7,14) },
   {id:'e2', title: 'New tv',price: 799.49, date: new Date(2021,2,12) },
   {id:'e3', title: 'Toilet Paper',price: 94.12, date: new Date(2021,2,28) },
   {id:'e4', title: 'new Desk',price: 450, date: new Date(2021,5,12) },
 ];
 
+const App = () => {
+
+const[expenses, setExpenses] = useState(INIT_EXPENSES);
 const addExpenseHandler = expense =>{
-  console.log("In App.js");
-  console.log(expense);
-}
+  setExpenses(prevExpenses =>{
+    return [expense, ...prevExpenses];
+  });
+};
 
   return (
     <div>
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,19 @@
-import { useState } from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(data) {
 
-  const [title, setTitle] = useState(data.title);
-
-  const clickHandler = () => {
-    setTitle('Updated title!');
-  }
- 
-
+  //const [title, setTitle] = useState(data.title);
+  
   return (    
     <Card className="expense-item">
       <ExpenseDate date={data.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{data.title}</h2>
         <div className="expense-item__price">${data.price}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
