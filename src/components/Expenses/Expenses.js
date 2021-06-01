28f5d9ee320345205c063355c20ac47b9cfd98bc
Kacper Makowski <kacper.makowski1@gmail.com>
2021-06-01 15:12:06 +0200

    Create ExpensesItem with map method

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,10 +15,13 @@ const Expenses = (props) =>{
     return(        
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onGetFilter={getFilterData} />
-            <ExpenseItem title={props.items[0].title} price={props.items[0].price} date={props.items[0].date}/>
-            <ExpenseItem title={props.items[1].title} price={props.items[1].price} date={props.items[1].date}/>
-            <ExpenseItem title={props.items[2].title} price={props.items[2].price} date={props.items[2].date}/>
-            <ExpenseItem title={props.items[3].title} price={props.items[3].price} date={props.items[3].date}/>
+            {props.items.map(expense => 
+                <ExpenseItem 
+                    key={expense.id} 
+                    title={expense.title} 
+                    price={expense.price} 
+                    date={expense.date} /> 
+            )}
         </Card>
     );
 }
