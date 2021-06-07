d28f88b7552acf3b8d00e7a9eb2dd201cf3172c7
Kacper Makowski <kacper.makowski1@gmail.com>
2021-06-07 14:13:28 +0200

    Bug fixing #1 string numbers in array instead of number numbers in array

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -50,7 +50,7 @@ const ExpenseForm = (props) =>{
 
         const expenseData = {
             title: enteredTitle,
-            price: enteredPrice,
+            price: +enteredPrice,
             date: new Date(enteredDate)
         };
         props.onFormData(expenseData);
