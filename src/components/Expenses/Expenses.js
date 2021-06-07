4f9205253547cd8ef2a69e2bd0f26e5e3d29deaf
Kacper Makowski <kacper.makowski1@gmail.com>
2021-06-07 13:53:50 +0200

    Create chart bars that's represents monthly expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,28 @@
-import Card from '../UI/Card';
-import './Expenses.css';
-import ExpensesFilter from './ExpensesFilter';
-import { useState } from 'react';
-import ExpensesList from './ExpensesList';
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
-const Expenses = (props) =>{
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
 
-    const [filteredYear, setFilteredYear] = useState('2021');
+  const getFilterData = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-    const getFilterData = (selectedYear) =>{
-        setFilteredYear(selectedYear);
-    }
+  const filteredData = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
-    const filteredData = props.items.filter(expense => {
-        return expense.date.getFullYear().toString() === filteredYear;
-    })
-
-    return(        
-        <Card className="expenses">
-            <ExpensesFilter selected={filteredYear} onGetFilter={getFilterData} />
-            <ExpensesList items={filteredData} />
-        </Card>
-    );
-}
+  return (
+    <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onGetFilter={getFilterData} />
+      <ExpensesChart expenses={filteredData} />
+      <ExpensesList items={filteredData} />
+    </Card>
+  );
+};
 
 export default Expenses;
